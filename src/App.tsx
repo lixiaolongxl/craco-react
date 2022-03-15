32520922033c81c4399dcd6f26a7c44e318e2710
lixiaolongxl <lxldevtest@163.com>
2022-03-15 11:54:48 +0800

    mians

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.less';
-import styles from './person.module.css';
+import styles from '@style/index.module.css';
 import { Button } from 'antd';
 import logo from './static/imges/logo192.png'
 import bg1 from './static/imges/bg.png'
 const logo1 = require('./static/imges/logo192.png')
 function App() {
-  console.log(process.env.REACT_APP_API_HOST)
+  // console.log(process.env.REACT_APP_API_HOST)
   return (
     <div className="App" >
-      <img src="./logo192.png" alt="" />
       <img src={logo} alt="" />
       <img src={logo1} alt="" />
       <div className={styles.box}>3233</div>
